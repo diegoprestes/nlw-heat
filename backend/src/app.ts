ca16e033b5dde70c6dcdbca0d13ab4f7040f245a
Diego Prestes <diego.prestes@alphasights.com>
2021-10-19 09:11:59 -0300

    feat: backend day

---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import express, { response } from "express";
+import { router } from "./routes";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected on socket ${socket.id}`);
+});
+
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+
+  return res.json(code);
+});
+
+export { serverHttp, io };
---
+++ backend/src/controllers/CreateMessageController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreateMessageController {
+  async handle(req: Request, res: Response) {
+    const { user_id } = req;
+    const { message } = req.body;
+    const service = new CreateMessageService();
+    try {
+      const result = await service.execute(message, user_id);
+
+      return res.json(result);
+    } catch (err) {
+      return res.json({ error: err.message });
+    }
+  }
+}
+
+export { CreateMessageController };
